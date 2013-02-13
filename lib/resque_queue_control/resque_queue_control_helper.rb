6a70cdc5c7f28a9b49832616de1a81ee7408c6c1
Christian Schulze <christian.schulze@gmail.com>
2013-02-13 11:15:28 +1100

    add super pause helper methods

--- lib/resque_queue_control/resque_queue_control_helper.rb
+++ lib/resque_queue_control/resque_queue_control_helper.rb
@@ -12,12 +12,31 @@ module ResqueQueueControlHelper
       Resque.redis.del "pause:queue:#{queue}"
     end
 
+    def super_paused?(queue)
+      !Resque.redis.get("super_pause:queue:#{queue}").nil?
+    end
+
+    def super_pause(queue)
+      Resque.redis.set "super_pause:queue:#{queue}", true
+    end
+
+    def super_unpause(queue)
+      Resque.redis.del "super_pause:queue:#{queue}"
+    end
+
     def locked?(queue)
       Resque.redis.exists "lonely_job:#{queue}"
     end
 
     def unlock(queue)
       Resque.redis.del "lonely_job:#{queue}"
+    end
+
+    def can_reserve?(queue)
+      return false if paused?(queue)
+      return false if super_paused?(queue)
+      return false if locked?(queue)
+      true
     end
   end
 end
